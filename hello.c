ae1d7e193807da17cd13f640cc482af074810683
Imants Keiselis <keiselis96@gmail.com>
2021-03-29 01:40:48 +0300

    Start

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World");
+}
+// machines use binary code to communicate
+// we will use built in cmd on ide.cs50.io
+// eg. ./hello to run
+//  to execute program we type: make filename in cmd
